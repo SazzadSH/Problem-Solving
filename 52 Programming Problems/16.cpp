8ec6cdafd414eda55ce711e6b36fc8a33e734a6f
Sazzad Hossen <thesazzadsh@gmail.com>
2018-05-13 13:18:23 +0600

    52 programming problems, 16.cpp

--- 52 Programming Problems/16.cpp
+++ 52 Programming Problems/16.cpp
@@ -1,16 +1,55 @@
 #include <iostream>
 #include <cstdio>
+#include <cstring>
 
 using namespace std;
+
+void reversePrint(char str[])
+{
+    int len = strlen(str);
+
+    for(int i = (len - 1); i >= 0; i++)
+    {
+        printf("%c", str[i]);
+    }
+    printf(" ");
+}
 
 int main()
 {
     int t;
-    char s[10];
-    string n;
+    char str[1002], word[1002];
 
-    cin >> s;
-    n = s;
-    cout << n << endl;
+    scanf("%d", &t);
+
+    while(t--)
+    {
+        scanf(" %[^\n]", str);
+
+        int len = strlen(str), i = 0, j = 0;
+
+        for(i = 0, j = 0; i < len; i++)
+        {
+            if(str[i] != ' ')
+            {
+                word[j] = str[i];
+                j++;
+            }
+            else if(j > 0)
+            {
+                word[j] = '\0';
+                reversePrint(word);
+                j = 0;
+            }
+        }
+
+        if(j > 0)
+        {
+            word[j] = '\0';
+            reversePrint(word);
+            printf("\n");
+        }
+    }
+
     return 0;
 }
