688ad462b6c5d449ea73f6588a298bff7c57e284
SazzadSH <thesazzadsh@gmail.com>
2020-03-05 20:52:22 +0600

    Java Stdin and Stdout I.java

---
+++ HackerRank/Java/Introduction/Java Stdin and Stdout I.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        //Input Variables
+        int firstInt, secondInt, thirdInt;
+
+        //Java Standard Input
+        Scanner scanner = new Scanner(System.in);
+        firstInt = scanner.nextInt();
+        secondInt = scanner.nextInt();
+        thirdInt = scanner.nextInt();
+        scanner.close();
+
+       //Java Standard Output
+        System.out.println(firstInt);
+        System.out.println(secondInt);
+        System.out.println(thirdInt);
+    }
+}
