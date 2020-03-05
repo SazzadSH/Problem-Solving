d298014f27afe817c84c09cc7074d01fff465020
SazzadSH <thesazzadsh@gmail.com>
2020-03-05 20:49:49 +0600

    Hackerrank/Java/Introduction/Welcome to Java!

---
+++ HackerRank/Java/Introduction/Welcome to Java!.java
@@ -0,0 +1,9 @@
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        //Standard Java Output
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
