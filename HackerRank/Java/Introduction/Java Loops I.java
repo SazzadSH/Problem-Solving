4a84f809c4251a67a8d6bbd6afa9dddb608201d7
SazzadSH <thesazzadsh@gmail.com>
2020-03-06 12:39:03 +0600

    Hackerrank/Java/Introduction/Java Loops I.java

---
+++ HackerRank/Java/Introduction/Java Loops I.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        int n;
+
+        Scanner scan = new Scanner(System.in);
+        n = scan.nextInt();
+
+        for(int i = 1; i <= 10; i ++)
+        {
+            System.out.println(n + " x " + i + " = " + (n*i));
+        }
+    }
+}
