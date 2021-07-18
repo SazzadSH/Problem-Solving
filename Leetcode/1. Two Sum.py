0b8b3c7c131afd0e38a57a91d953da7948ed0dcd
Sazzad Hossen <thesazzadsh@gmail.com>
2021-07-18 14:06:20 +0600

    leetcode 1. two sum

--- Leetcode/1. Two Sum.py
+++ Leetcode/1. Two Sum.py
@@ -1,8 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in nums:
-            for j in nums:
-                if i ==  j:
-                    continue
-                if nums[i] + nums[j] == target:
+        numsLength = len(nums)
+
+        #iterate the list to find index containing solution
+        for i in range(numsLength):
+            j = i + 1
+            while j < numsLength:
+                if ((nums[i] + nums[j]) == target):
+                    #valid solution
                     return [i, j]
+                j += 1
